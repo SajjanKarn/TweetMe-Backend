29de28f3ade17a95e456f792676a799733e9efa3
SajjanKarn <49333264+SajjanKarn@users.noreply.github.com>
2021-06-08 18:33:41 +0545

    added cors support

--- startup/middleware-integration.js
+++ startup/middleware-integration.js
@@ -7,6 +7,20 @@ module.exports = (app) => {
   // adds JSON support to our express app.
   app.use(express.json());
 
+ app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
   // enable cors.
   app.use(require("cors")());
 
